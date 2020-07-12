70c345d5f1dc1557b872e68c2db7139f71f31f27
DanielSoares96 <32708212+DanielSoares96@users.noreply.github.com>
2020-07-12 17:57:37 +0200

    12/07

--- myProject/blog/views.py
+++ myProject/blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import post
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts' : post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
